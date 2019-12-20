fc9a0c6c5d3e1779ba11a585e14d8717cc111669
Rodolfo Alvarenga <rodolfo.l.alvarenga@gmail.com>
2019-12-19 23:47:40 -0300

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,20 @@
+const key = 'IR4260Q6YK30AhZFQ8YC1z4uW2FidOUx';
+
+//get weather information
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+
+// get city information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+
