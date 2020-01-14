ef8f8d2a998fd4c37a73eb5c7464391eaa489b4c
Rodolfo Alvarenga <rodolfo.l.alvarenga@gmail.com>
2020-01-14 12:13:05 -0300

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,7 @@ const key = 'IR4260Q6YK30AhZFQ8YC1z4uW2FidOUx';
 
 //get weather information
 const getWeather = async (id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
     const response = await fetch(base + query);
     const data = await response.json();
@@ -11,7 +11,7 @@ const getWeather = async (id) => {
 
 // get city information
 const getCity = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
     const response = await fetch(base + query);
     const data = await response.json();
